df388bd531a30fe762bf2915844caf2a44baf825
Abhay <hackyabhay@gmail.com>
2024-09-23 12:52:45 +0530

    added ton

--- src/Pages/MemeStickers.jsx
+++ src/Pages/MemeStickers.jsx
@@ -1,28 +1,24 @@
 import React, { useState, useEffect, useCallback } from "react";
+import { useTonConnectUI } from '@tonconnect/ui-react';
 import logo from "../Assets/Logo.webp";
 import { motion } from "framer-motion";
 import { useParams } from "react-router-dom";
 import { doc, setDoc, getDoc, updateDoc, arrayUnion } from "firebase/firestore";
-import { db } from "../FireBase"; // Import Firestore instance
-import { debounce } from "./debounce"; // Import custom debounce function
+import { db } from "../FireBase"; // Firestore instance
+import { debounce } from "./debounce"; // Custom debounce function
 import { CiWallet } from "react-icons/ci";
 
 const MemeStickers = () => {
   const [count, setCount] = useState(0);
   const [isAnimating, setIsAnimating] = useState(false);
-  const {
-    chatid: paramChatid,
-    username: paramUsername,
-    referralid,
-  } = useParams(); // Get values from route params
+  const [tonConnectUI] = useTonConnectUI();
+  const { chatid: paramChatid, username: paramUsername, referralid } = useParams();
 
-  // Retrieve chatid and username from localStorage if not available in params
   const chatid = paramChatid || localStorage.getItem("chatid");
   const username = paramUsername || localStorage.getItem("username");
 
   console.log(referralid);
 
-  // Save chatid and username in localStorage if available
   useEffect(() => {
     if (chatid) {
       localStorage.setItem("chatid", chatid);
@@ -31,9 +27,8 @@ const MemeStickers = () => {
       localStorage.setItem("username", username);
       localStorage.setItem("totalCoinsMined", count);
     }
-  }, [chatid, username]);
+  }, [chatid, username, count]);
 
-  // Save data to Firestore or update if it exists
   useEffect(() => {
     const saveDataToFirestore = async () => {
       try {
@@ -55,11 +50,14 @@ const MemeStickers = () => {
       }
     };
 
-    saveDataToFirestore();
+    if (chatid && username) {
+      // Ensure chatid and username have values before attempting to save
+      saveDataToFirestore();
+    }
   }, [chatid, username]);
 
   useEffect(() => {
-    if (referralid) {
+    if (referralid && chatid) {
       addInvitedPerson(chatid, referralid);
     }
   }, [referralid, chatid]);
@@ -75,7 +73,6 @@ const MemeStickers = () => {
     }
   };
 
-  // Create a debounced version of the updateMinedCoins function
   const debouncedUpdateMinedCoins = useCallback(
     debounce(async (chatid, newCoins) => {
       try {
@@ -84,7 +81,7 @@ const MemeStickers = () => {
       } catch (error) {
         console.error("Error updating document:", error);
       }
-    }, 3000), // Debounce delay in milliseconds
+    }, 3000),
     []
   );
 
@@ -92,17 +89,26 @@ const MemeStickers = () => {
     setCount((prev) => prev + 1);
     setIsAnimating(true);
 
-    // Delay setting isAnimating to false to allow animation to complete
     setTimeout(() => {
       setIsAnimating(false);
-    }, 500); // Duration of shake effect
+    }, 500);
 
     debouncedUpdateMinedCoins(chatid, count + 1);
   };
 
+  const handleConnectWallet = () => {
+    if (tonConnectUI.connected) {
+      tonConnectUI.disconnect();
+    } else {
+      tonConnectUI.connectWallet()
+        .catch(error => {
+          console.error('Failed to connect the wallet:', error);
+        });
+    }
+  };
+
   return (
     <div className="bg-[#046be2] min-h-screen flex flex-col justify-between text-black">
-      {/* Header Section */}
       <div className="flex flex-col justify-between h-[30%] items-start p-3 sm:p-4">
         <div className="w-full flex items-center justify-between">
           <div className="flex items-center pl-5 gap-2">
@@ -115,22 +121,27 @@ const MemeStickers = () => {
             </div>
           </div>
           <div>
-            <button className="text-black font-sans bg-white rounded-3xl text-sm  px-3 py-2 font-medium ">
-              Connect
+            <button
+              onClick={handleConnectWallet}
+              className="text-black font-sans bg-white rounded-3xl text-sm px-3 py-2 font-medium"
+            >
+              {tonConnectUI.connected ? 'Disconnect' : 'Connect'}
             </button>
           </div>
         </div>
       </div>
 
-      {/* Main Section */}
-      <div className="flex flex-col    flex-grow  justify-evenly    bg-black rounded-t-2xl items-center ">
+      <div className="flex flex-col flex-grow justify-evenly bg-black rounded-t-2xl items-center">
         <div>
-          <button className="text-black  font-sans flex gap-1 items-center bg-white rounded-full p-2 mt-2 text-sm font-semibold px-3 ">
+          <button
+            onClick={handleConnectWallet}
+            className="text-black font-sans flex gap-1 items-center bg-white rounded-full p-2 mt-2 text-sm font-semibold px-3"
+          >
             <CiWallet />
-            Connect wallet
+            {tonConnectUI.connected ? 'Disconnect wallet' : 'Connect wallet'}
           </button>
         </div>
-        <div className="w-full  flex flex-col items-center justify-between">
+        <div className="w-full flex flex-col items-center justify-between">
           <motion.img
             onClick={handleBatClick}
             src={logo}
@@ -150,20 +161,21 @@ const MemeStickers = () => {
             </motion.div>
           )}
           <div className="pl-3 flex-col flex items-center p-1 justify-between rounded-md">
-            <p className=" bg-gradient-to-r  font-sans from-gray-400 via-gray-300 to-gray-200 bg-clip-text text-transparent font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
+            <p className="bg-gradient-to-r font-sans from-gray-400 via-gray-300 to-gray-200 bg-clip-text text-transparent font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
               {count}
             </p>
-            <p className="text-gray-300 uppercase  font-sans text-lg">$Bugs</p>
-
+            <p className="text-gray-300 uppercase font-sans text-lg">$Bugs</p>
           </div>
         </div>
 
-        {/* Animation & Logo */}
-        <div className="w-full h-52 flex flex-col items-center justify-start  relative">
-          <button className="bg-white  font-sans mt-12 w-[90%] font-bold text-black text-md py-2 rounded-xl">
-            Connect wallet and claim
+        <div className="w-full h-52 flex flex-col items-center justify-start relative">
+          <button
+            onClick={handleConnectWallet}
+            className="bg-white font-sans mt-12 w-[90%] font-bold text-black text-md py-2 rounded-xl"
+          >
+            {tonConnectUI.connected ? 'Disconnect wallet' : 'Connect wallet and claim'}
           </button>
-          <button className="bg-[#1c1c1e]  font-sans  mt-3 w-[90%] font-bold text-white text-md py-2 rounded-xl">
+          <button className="bg-[#1c1c1e] font-sans mt-3 w-[90%] font-bold text-white text-md py-2 rounded-xl">
             Claim hints
           </button>
         </div>
@@ -173,11 +185,3 @@ const MemeStickers = () => {
 };
 
 export default MemeStickers;
-
-// <div className="w-full flex items-center justify-center">
-//   <div className="px-10 flex items-center p-1 justify-center rounded-md">
-//     <p className="capitalize bg-gradient-to-r from-gray-400 via-gray-300 to-gray-200 bg-clip-text text-transparent font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
-//       {count}
-//     </p>
-//   </div>
-// </div>
